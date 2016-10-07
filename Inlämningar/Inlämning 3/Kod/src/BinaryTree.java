e15a0c8616f0660ab9273af694d05e53d390e130
Robin Söderholm <robin.soderholm87@gmail.com>
2016-10-07 12:02:32 +0200

    Klasser

---
+++ Inlämningar/Inlämning 3/Kod/src/BinaryTree.java
@@ -0,0 +1,9 @@
+
+
+/**
+ * Created by robin on 2016-10-07.
+ */
+public class BinaryTree {
+    private TreeNode root;
+    private int size;
+}
---
+++ Inlämningar/Inlämning 3/Kod/src/TreeNode.java
@@ -0,0 +1,13 @@
+/**
+ * Created by robin on 2016-10-07.
+ */
+public class TreeNode {
+    private int data;
+    private TreeNode left;
+    private TreeNode right;
+
+    public TreeNode(int data) {
+        this.data = data;
+
+    }
+}
